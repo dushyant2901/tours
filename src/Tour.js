d0e50f49f12c2490cd38c8f9fa41a15bb9910cb6
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-01-11 23:24:41 +0530

    style css modification

--- src/Tour.js
+++ src/Tour.js
@@ -1,16 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = ({id,name,info,price,image,removeTour}) => {
-  const [readMore, setReadMore] = useState(false)
+const Tour = ({ id, name, info, price, image, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
-    <article className='single-tour'>
-     <img src={image} alt={name} />
-    <footer> <div className="tour-info"> <h3>{name}</h3> <h3 className='tour-price'>{price}</h3></div>
-     <p>{readMore?info: `${info.substring(0,200)}...`}
-     <button onClick={()=>setReadMore(!readMore)}>{readMore?"show less ":'show more'}</button></p>
-     <button className='delete-btn' onClick={()=>removeTour(id)}>Not Interested</button></footer>
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        {" "}
+        <div className="tour-info">
+          {" "}
+          <h4>{name}</h4> <h4 className="tour-price">{price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "show less " : "show more"}
+          </button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          Not Interested
+        </button>
+      </footer>
     </article>
-  )
+  );
 };
 
 export default Tour;
